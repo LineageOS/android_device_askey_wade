5a054c96336def7d526bcdc3e8075165b5f0d4b8
Nolen Johnson <johnsonnolen@gmail.com>
2021-06-17 00:43:04 -0400

    wade: Initial Commit
    Change-Id: I5bbc92935d81897929edf3daefb27a9e09691f60

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_wade.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_wade-eng \
+    lineage_wade-user \
+    lineage_wade-userdebug
---
+++ lineage_wade.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common AOSP stuff
+$(call inherit-product, device/google/atv/products/atv_base.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Askey
+PRODUCT_DEVICE := wade
+PRODUCT_GMS_CLIENTID_BASE := android-askey-tv
+PRODUCT_MANUFACTURER := Askey
+PRODUCT_MODEL := Dynalink TV Box
+PRODUCT_NAME := lineage_wade
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=wade \
+    PRIVATE_BUILD_DESC="asti6130d350-user 10 QT 20210326 release-keys"
+
+BUILD_FINGERPRINT := Dynalink/sti6130d350/sti6130d350:10/QT/20210326:user/release-keys
