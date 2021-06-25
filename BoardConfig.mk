283eecb8f2537dc7c0ced79f3a9dac5a51f3417f
Nolen Johnson <johnsonnolen@gmail.com>
2021-06-25 01:05:35 -0400

    wade: Configure Broadcom Bluetooth/Wi-Fi HALs
    * We're not unifying things, and the stock MultiHAL requires
      framework side magic, so we're going with the hardware/broadcom
      solution.
    
    * Set Bluetooth overlays based on stock.
    
    * bdroid_buildcfg header and vnd_$device.txt configuration are
      loosely taken from OEM uploads of Amlogic BCM Wi-Fi/BT device
      sources.
    
    Change-Id: Ie8e0f2d2a5f1cb8337ebe88b903fb18a71602f18

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,9 +10,25 @@ include device/amlogic/g12-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/askey/wade
 
+## Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+BOARD_CUSTOM_BT_CONFIG := $(DEVICE_PATH)/bluetooth/vnd_wade.txt
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
 ## Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := wade
 
 ## DTB
 TARGET_DTB_NAME := g12a_s905x2_u212_sti6130d3x0
 TARGET_DTBO_NAME := sti6140d350_overlay
+
+## Wi-Fi
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_AP := "/vendor/etc/wifi/buildin/fw_fw_bcm4356a2_ag_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/etc/wifi/buildin/fw_bcm4356a2_ag.bin"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
