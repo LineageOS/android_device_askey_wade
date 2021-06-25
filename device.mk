efe305d562a9d96da544e29a68ffc8a307d2f3e6
Aaron Kling <webgeek1234@gmail.com>
2021-06-25 01:04:25 -0400

    wade: Build Broadcom DHD module inline
    Change-Id: I042931d6361420868f53f682c70fee8c5a2b589d

--- device.mk
+++ device.mk
@@ -9,3 +9,7 @@ DEVICE_PATH := device/askey/wade
 $(call inherit-product, vendor/askey/wade/wade-vendor.mk)
 
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
+
+## Kernel Modules
+PRODUCT_PACKAGES += \
+    dhd
