e84a183a961caaaa6485b0b2c15b5a3e5fdb2dee
Aaron Kling <webgeek1234@gmail.com>
2021-09-01 16:06:46 -0500

    wade: Add netflix config overlay
    This replicates the stock configs
    
    Based on example by luca020400
    
    Change-Id: Iad4e13a40efe64d4f999abc93d6a41ffd149e3f6

---
+++ Android.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),wade)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+endif
