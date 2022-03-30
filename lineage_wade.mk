fb73de9ffa45c006227967c8bd3ce55e9cde1c6b
Rohan Hasabe <rohanhasabe8@gmail.com>
2022-03-30 13:33:31 +0200

    wade: Update blobs to sti6130d350-user-10-QT-20220222
    Change-Id: Ic25fbb63a737f562659c7d0186650f41242f72d8

--- lineage_wade.mk
+++ lineage_wade.mk
@@ -23,6 +23,6 @@ PRODUCT_NAME := lineage_wade
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=wade \
-    PRIVATE_BUILD_DESC="asti6130d350-user 10 QT 20211207 release-keys"
+    PRIVATE_BUILD_DESC="asti6130d350-user 10 QT 20220222 release-keys"
 
-BUILD_FINGERPRINT := Dynalink/sti6130d350/sti6130d350:10/QT/20211207:user/release-keys
+BUILD_FINGERPRINT := Dynalink/sti6130d350/sti6130d350:10/QT/20220222:user/release-keys
