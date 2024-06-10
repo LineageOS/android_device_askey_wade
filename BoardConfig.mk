ce3e6e42140c47a692856a4c9c8efe2ee9c2a593
Nolen Johnson <johnsonnolen@gmail.com>
2024-06-10 15:39:41 -0400

    wade: Allow droidlogic app to interact with systemcontrol hwservice
    * No idea how stock does this, will figure it out later.
    * Labeling droidlogic app changes nothing.
    
    Change-Id: Icd27d73402a9fdffdc849de7d2f56183e86e3da6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,10 @@ BOARD_SUPER_PARTITION_SIZE := 1677721600
 ## Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+## SELinux
+SELINUX_IGNORE_NEVERALLOWS := true
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/sabrina/sepolicy/vendor
+
 ## Wi-Fi
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE := bcmdhd
