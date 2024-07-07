3a313a6773c709faccb679a5de166080d283581e
Bruno Martins <bgcngm@gmail.com>
2024-07-07 18:03:11 +0100

    wade: Set new BT and WiFi firmware include paths
    Change-Id: I9d9d34e4636d294bb6e03d37e2eb78cacea4f5ad

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,6 +8,9 @@
 PRODUCT_PACKAGES += \
     WadeBluetoothOverlay \
     libbt-vendor
+
+## Bluetooth firmware
+include kernel/amlogic/kernel-modules/dhd-driver/firmware/bluetooth/bluetooth.mk
 
 ## GMS
 ifeq ($(WITH_GMS),true)
@@ -18,8 +21,8 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
 
-## Wi-Fi Firmware
-include hardware/amlogic/kernel-modules/dhd-driver/firmware/firmware.mk
+## Wi-Fi firmware
+include kernel/amlogic/kernel-modules/dhd-driver/firmware/wifi/wifi.mk
 
 ## Inherit from the common tree product makefile
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
