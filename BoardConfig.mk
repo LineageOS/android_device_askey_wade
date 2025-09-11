9c4f12cbf77a074e25cba59a4c1b25870066a90b
Nolen Johnson <johnsonnolen@gmail.com>
2025-09-11 01:19:42 -0400

    wade: Adapt to libbt migration to blueprint
    Change-Id: I1acc5a1ed5eb18608980c02af719fcc08d0cfeeb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,10 +7,9 @@
 DEVICE_PATH := device/askey/wade
 
 ## Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
-BOARD_CUSTOM_BT_CONFIG := $(DEVICE_PATH)/bluetooth/vnd_wade.txt
 BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
+$(call soong_config_set,brcm_libbt,bdroid_buildcfg_include_dir,$(DEVICE_PATH)/bluetooth/include)
+$(call soong_config_set,brcm_libbt,custom_bt_config,//device/askey/wade:vnd_wade.txt)
 
 ## Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := wade
