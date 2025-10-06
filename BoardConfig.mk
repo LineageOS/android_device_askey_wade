7e77959b0fa713a2d87553b4dec954baa5d021fb
Nolen Johnson <johnsonnolen@gmail.com>
2025-10-06 16:39:18 +0000

    Revert "wade: Switch to bcmdhd.101.10.591.x driver"
    This reverts commit 84aad072c102825b5adb38013d5c2f58ff2237b1.
    
    Reason for revert: Breaks things.
    
    Change-Id: Ia1966957d63a24e397cb73662684fe7cd1c4ff20

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 ## Kernel modules
 TARGET_KERNEL_EXT_MODULES := \
-    dhd-driver/bcmdhd.101.10.591.x
+    dhd-driver/bcmdhd.101.10.361.x
 
 ## Partitions
 BOARD_SUPER_PARTITION_SIZE := 1677721600
