557d16578fa0a70520a967048241e2cc0dc87717
Bruno Martins <bgcngm@gmail.com>
2026-08-29 12:41:50 +0100

    wade: Rename and migrate init to blueprint
    Change-Id: I0013cac7636849924b0783775f22cfc379e470b3

--- device.mk
+++ device.mk
@@ -19,10 +19,10 @@ ifeq ($(WITH_GMS),true)
 GMS_MAKEFILE=gms_minimal.mk
 endif
 
-## Init-Files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc \
-    $(LOCAL_PATH)/init-files/init.amlogic.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.target.rc \
+## Init
+PRODUCT_PACKAGES += \
+    init.amlogic.wifi_buildin.rc \
+    init.amlogic.target.rc
 
 ## IR
 PRODUCT_PACKAGES += \
